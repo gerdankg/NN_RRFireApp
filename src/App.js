0bec80111098113109e5024360c96a7932dce29e
unknown <gerdankg@gmail.com>
2020-10-16 00:02:48 +0600

    Create new Navbarcomponents

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React,{Component} from 'react';
 import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
 
-import './Index.css';
+import './index.css';
 
 class App extends Component {
   render(){
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>My plan</h1>
+      <Navbar/>
     </div>
     </BrowserRouter>
   );
---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom' /// NavLink getting class
+
+
+
+const SignedInLinks = () => {
+    return (
+       <ul className="right">
+           <li><NavLink to='/'>New project</NavLink></li>
+           <li><NavLink to='/'>Log out</NavLink></li>
+           <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+       </ul>
+    )
+}
+
+export default SignedInLinks;
